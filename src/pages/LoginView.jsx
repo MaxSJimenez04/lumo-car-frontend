9e052bc2dc5cc381cb23bb85f1b1e761c6c31330
MaxSJimenez04 <maximiliano.soto.j@gmail.com>
2026-06-06 12:08:26 -0600

    Creacion de páginas con sus rutas

--- src/pages/LoginView.jsx
+++ src/pages/LoginView.jsx
@@ -13,12 +13,10 @@ export default function LoginView() {
                 <h1 className="lumo-logo">
                     LUMO
                 </h1>
-                //Se utilizan los componentes para construir la ventana
                 <GlassCard
                     title="Iniciar Sesión"
                     subtitle="¡Feliz de que estés de vuelta!"
                 >
-                    //Adentro de la glass card se coloca el formulario para que vaya adentro
                     <LoginForm />
                 </GlassCard>
 
