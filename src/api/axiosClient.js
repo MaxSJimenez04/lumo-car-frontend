52239d60fd22c19ca70b5128e67e900242ceccad
MaxSJimenez04 <maximiliano.soto.j@gmail.com>
2026-06-08 18:39:48 -0600

    fix: arreglar inyección de JWT

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -6,7 +6,7 @@ const api = axios.create({
 })
 
 api.interceptors.request.use((config) =>{
-    const token = localStorage.getItem("token") //Se consulta si el token ya está en el LocalStorage
+    const token = localStorage.getItem("jwt") //Se consulta si el token ya está en el LocalStorage
 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
